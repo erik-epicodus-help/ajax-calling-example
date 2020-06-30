72f2c768768080a94258a8905310623adfc93805
Erik Irgens <erik.t.irgens@gmail.com>
2020-06-29 20:46:27 -0700

    Weather homework complete

---
+++ src/lessonplan.js
@@ -0,0 +1,18 @@
+// AJAX - Asynchronous JavaScript And XML.
+
+// AJAX uses a combo of 
+// + A browser built -in XMLHttpRequest object(to request data from a web server)
+// + JavaScript and HTML DOM(to display or use the data)
+
+// 1. An event occurs in a web page(the page is loaded, a button is clicked)
+// 2. An XMLHttpRequest object is created by JavaScript
+// 3. The XMLHttpRequest object sends a request to a web server
+// 4. The server processes the request
+// 5. The server sends a response back to the web page
+// 6. The response is read by JavaScript
+// 7. Proper action(like page update) is performed by JavaScript
+
+// API key is necessary for you to access certain closed sources. There are some APIs that do /not/ require an API key. 
+
+// Postman can be as simple as you want it to be. You could do a "GET" request on wikipedia.com, and it would return something. Play around!
+
